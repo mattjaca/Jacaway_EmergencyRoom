7009f91befdef3235e95b774bfee49825e8441f5
Matthew Jacaway <matthew.jacaway@student.centralia.edu>
2023-12-04 11:53:08 -0800

    Added Files

---
+++ Jacaway_EmergencyRoom/Patient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jacaway_EmergencyRoom
+{
+    internal class Patient
+    {
+        private string FirstName;
+        private string LastName;
+        private int Priority;
+        DateOnly Birthdate = new DateOnly();
+        public Patient(string firstName, string lastName, int priority, DateOnly birthdate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Priority = priority;
+            Birthdate = birthdate;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Jacaway_EmergencyRoom/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Jacaway_EmergencyRoom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PriorityQueue<Patient, int> ERQueue = new PriorityQueue<Patient, int>();
+
+        }
+    }
+}
